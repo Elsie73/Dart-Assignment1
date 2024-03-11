bdcf9789d6e0dd6a59823eb52c35e7ae6eedbd77
Elsie73 <160402110+Elsie73@users.noreply.github.com>
2024-03-11 09:18:54 +0300

    Add files via upload

---
+++ DataTypes.dart
@@ -0,0 +1,23 @@
+// Dart program demonstrating various data types
+
+void main() {
+  // Integer
+  int myInt = 10;
+  print('Integer: $myInt');
+
+  // Double
+  double myDouble = 3.14;
+  print('Double: $myDouble');
+
+  // String
+  String myString = 'Hello, i am Elsie!';
+  print('String: $myString');
+
+  // List
+  List<int> myList = [1, 2, 3, 4, 5];
+  print('List: $myList');
+
+  // Map
+  Map<String, int> myMap = {'apple': 5, 'banana': 10, 'orange': 7};
+  print('Map: $myMap');
+}
